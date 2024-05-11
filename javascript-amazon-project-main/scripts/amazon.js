1f3baf34c53a2a31e0c35d95a3a10d205d9b1af0
TheRealLeonZZ <140121089+TheRealLeonZZ@users.noreply.github.com>
2024-05-11 23:46:58 +0300

    added 2 second delay of 'added to cart'

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -41,7 +41,7 @@ products.forEach((product) => {
 
     <div class="product-spacer"></div>
 
-    <div class="added-to-cart">
+    <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
     </div>
@@ -91,5 +91,16 @@ allAddToCartButtons.forEach((button, index) => {
     });
 
     cartQuantityElement.innerHTML = `${cartQuantity}`;
+
+    // Show 'added to cart'
+    const addedToCartElement = document.querySelector(
+      `.js-added-to-cart-${productId}`
+    );
+
+    addedToCartElement.classList.add("added-to-cart-visible");
+
+    let addedToCartMsgTimeout = setTimeout(() => {
+      addedToCartElement.classList.remove("added-to-cart-visible");
+    }, "2000");
   });
 });
