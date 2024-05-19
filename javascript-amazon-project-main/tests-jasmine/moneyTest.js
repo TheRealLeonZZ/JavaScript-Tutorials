d244ed2001f949b27541f59cd65953cbf28872c3
TheRealLeonZZ <140121089+TheRealLeonZZ@users.noreply.github.com>
2024-05-19 13:14:56 +0300

    Added describe inside a describe in jasmine

--- javascript-amazon-project-main/tests-jasmine/moneyTest.js
+++ javascript-amazon-project-main/tests-jasmine/moneyTest.js
@@ -9,11 +9,13 @@ describe("test suite: formatCurrency", () => {
     expect(formatCurrency(0)).toEqual("0.00");
   });
 
-  it("rounds up to the nearest cent", () => {
-    expect(formatCurrency(2000.5)).toEqual("20.01");
-  });
+  describe("Rounding", () => {
+    it("rounds up to the nearest cent", () => {
+      expect(formatCurrency(2000.5)).toEqual("20.01");
+    });
 
-  it("rounds down to the nearest cent", () => {
-    expect(formatCurrency(2000.4)).toEqual("20.00");
+    it("rounds down to the nearest cent", () => {
+      expect(formatCurrency(2000.4)).toEqual("20.00");
+    });
   });
 });
